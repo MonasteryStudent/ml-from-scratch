00cb4f2972077d31c40a1598282b04afba8cf267
cs_knoll <cs_knoll@proton.me>
2026-06-10 15:11:31 +0200

    Add univariate linear regression implementation and tests

---
+++ src/ml_from_scratch/linear_regression.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def predict(x, w, b):
+    return x * w + b
+
+def compute_cost(x, y, w, b):
+    m = len(x)
+    error = predict(x, w, b) - y
+    return np.sum(error**2) / (2*m)
+
+def gradient_descent(x, y, w, b, alpha, iterations):
+    m = len(x)
+    for _ in range(iterations):
+        error = predict(x, w, b) - y
+        dj_dw = np.sum(error * x) / m
+        dj_db = np.sum(error) / m
+        w_temp = w - alpha * dj_dw
+        b_temp = b - alpha * dj_db
+        w = w_temp
+        b = b_temp
+    return w, b
+
+def uni_linear_regression(x, y, alpha, iterations):
+    w = 0
+    b = 0
+    return gradient_descent(x, y, w, b, alpha, iterations)
